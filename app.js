212c1150b70a144b4d86cfdd980940c88c7b43b3
audalio-devops <audalio.devops@gmail.com>
2023-09-09 13:25:58 -0300

    criando a página de contato

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Conectando ao Banco de Dados")
 // outro comit de log
 console.log("O Audaliojr fez esse commit sem vc ver")
+console.log("Enviando para o GitHub")
+console.log("Iniciando Contato")
